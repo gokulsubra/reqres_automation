41505a69bdf332d4df1bf9bd35378e49f113a4ca
gokulsubra <157695610+gokulsubra@users.noreply.github.com>
2024-02-01 22:08:03 +0530

    Update getRequest.rb

--- features/step_defintions/getRequest.rb
+++ features/step_defintions/getRequest.rb
@@ -2,7 +2,7 @@ require_relative '../../common'
 
 Given('an api for to perform a get request') do
   puts "Starting the execution of step"
-  url = "https://dummy.restapiexample.com/api/v1/employees"
+  url = "https://reqres.in/api/users?page=2"
   response = Api.new.getMethod(url)
   responseBody = JSON.parse(response.body)
   puts "Response : #{responseBody}"
